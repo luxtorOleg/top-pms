1f5d44a8b60f0e03ae49b56f0d2e20d4b98c4c45
Oleg <olegozibko@gmail.com>
2018-04-26 20:58:01 +0300

    add RegistrationComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {NavbarComponent} from './navbar/navbar.component';
 import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'navbar', component: NavbarComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { NavbarService } from './navbar.service';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 
@@ -14,7 +15,8 @@ import { NavbarService } from './navbar.service';
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
